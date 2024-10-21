8c1642bc2fbde84507a6c0c9638a6cd364acbe21
jabedzaman <hi@jabed.dev>
2024-10-21 06:08:18 +0530

    feat: init drizzle orm

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,25 @@
 import { Hono } from "hono";
+import { prettyJSON } from "hono/pretty-json";
+import { logger } from "hono/logger";
+import { router } from "./routers/index.js";
+import { apiReference } from "@scalar/hono-api-reference";
 
 export const app = new Hono();
+
+app.get(
+  "/reference",
+  apiReference({
+    spec: {
+      url: "/openapi.json",
+    },
+  })
+);
+
+app.use(
+  prettyJSON({
+    space: 4,
+  })
+);
+app.use(logger());
+
+app.route("/", router);
